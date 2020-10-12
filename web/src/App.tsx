08ff581f4413e1cb6d694192903ab3ccf9c9fcba
wellpinho <wellpinho@outlook.com>
2020-10-12 15:51:41 -0300

    Finalizando a page home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import LogoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={LogoImg} alt="Happy!"/>
+
+        <main>
+          <h1>Leve Felicidade para o mundo!</h1>
+          <p>Visite orfanatos e alegre o dia de muitas crianças \o/ \o/ \o/</p>
+        </main>
+
+        <div className="location">
+          <strong>Rio de Janeiro </strong>
+          <span>RJ</span>
+        </div>
+
+          <a href="" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, .6)" />
+          </a>
+      </div>
     </div>
   );
 }
